5f2259eea61cd594009412754bfea4efbfa26f0e
lucieDLE <dolelucie0@gmail.com>
2026-06-17 09:44:03 -0400

    create keys temperature/ndvi indicators

--- app/data.py
+++ app/data.py
@@ -40,5 +40,51 @@ def load_pixels(scene):
     return pd.read_parquet(PROCESSED / scene / "lst_ndvi.parquet.gzip")
 
 
+def filter_residential_areas(gdf):
+    return gdf.loc[ gdf.ntatype == '0']
+
+def compute_indicators(gdf_temperature, df_lst_ndvi):
+
+    dict_indicators = {}
+
+    pearson_coeff = df_lst_ndvi['lst'].corr(df_lst_ndvi['ndvi'])
+    slope, intercept = np.polyfit(df_lst_ndvi["ndvi"], df_lst_ndvi["lst"], 1)
+
+    gdf_residential_temperature = filter_residential_areas(gdf_temperature)
+
+    df_coolest = gdf_temperature.sort_values(by=['lst_mean'])[['boroname', 'ntatype', 'ntaname', 'lst_mean', 'lst_std','ndvi_mean',  'ndvi_std'] ].dropna()
+
+    
+    df_lst_mean = gdf_residential_temperature.sort_values(by='lst_mean')[ ['borocode','boroname','ntaname','nb_id', 'lst_mean', 'ndvi_mean']]
+    df_lst_min_max_nb = pd.concat([df_lst_mean[:5], df_lst_mean[-5:]])
+    
+
+    df_lst_std = gdf_residential_temperature.sort_values(by=['lst_mean', 'ndvi_mean'], ascending=[False, True])[['boroname', 'ntaname', 'lst_mean', 'lst_std','ndvi_mean',  'ndvi_std'] ]
+    df_inequality = pd.concat([df_lst_std[:5],df_lst_std[-5:] ])
+    
+    # City level temperature values for that day
+    dict_indicators['City-Level Temperature'] = {
+        'mean': gdf_temperature.lst_mean.mean(),
+        'min': gdf_temperature.lst_min.min(),	
+        'max': gdf_temperature.lst_max.max(),
+    }
+    
+    # Answer Question 1: Do greener blocks mean cooler?
+    dict_indicators['Pearson'] = pearson_coeff
+    dict_indicators['Polyfit'] = (slope, intercept)
 
 
+    # Tile 2: plot Heat beside Green (LST/NDVI mean)
+    # plot gdf_type 0 for Heat but all ntatype for NDVI
+    dict_indicators['ranking Hottest/Coolest Residential'] = df_lst_min_max_nb
+
+
+    # Tile 3: Inequality (LST std /NDVI mean)
+    dict_indicators['ranking Heat Inequality'] = df_lst_min_max_nb
+
+
+    # Tile 1: Coolest Neighborhoods
+    # ---> parks with low temperature, high ndvi mean 
+    dict_indicators['ranking Heat Inequality'] = df_coolest.head(10)
+
+    return dict_indicators
